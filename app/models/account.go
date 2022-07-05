e6533118fb401595114b162c8fb3d9a66f3f8ddc
Gabriel Pôrto <gabrielporto1903@gmail.com>
2022-07-04 23:01:54 -0300

    add password hashing

--- app/models/account.go
+++ app/models/account.go
@@ -2,6 +2,8 @@ package models
 
 import (
 	"time"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type Account struct {
@@ -11,4 +13,22 @@ type Account struct {
 	Secret    string    `json:"secret"`
 	Balance   float64   `json:"balance"`
 	CreatedAt time.Time `json:"created_at"`
+}
+
+func (account *Account) HashPassword(password string) error {
+	hashed, err := bcrypt.GenerateFromPassword([]byte(account.Secret), 14)
+	if err != nil {
+		return err
+	}
+
+	account.Secret = string(hashed)
+	return nil
+}
+
+func (account *Account) CheckPassword(providedPassword string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(account.Secret), []byte(providedPassword))
+	if err != nil {
+		return err
+	}
+	return nil
 }
